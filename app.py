c1c595bd0f9ec5d0adee4998901acb745bec1e68
moti9 <motiky.iiitag@gmail.com>
2023-04-12 00:26:23 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from pymongo import MongoClient, ReturnDocument
 from jsonschema import validate
+from flask_restful import Api,Resource
 app = Flask(__name__)
+api=Api(app)
 
 # Connect to the database
 client = MongoClient('mongodb://localhost:27017')
@@ -29,99 +31,83 @@ user_schema={
   "required": ["id","name", "email", "password"]
 }
 
+class User(Resource):
+    # /users - Returns a list of users
+    def get(self,id=None):
+        # Single user
+        if id:
+            try:
+                user = collection.find_one({'id': id})
+                if user is None:
+                    return jsonify({'message': 'User not found'})
+                return jsonify({'id': user['id'], 'name': user['name'], 'email': user['email']})
+            except: 
+                return jsonify({"message": "No user found"})
+        # All users
+        else:
+            users=[]
+            try: 
+                for user in collection.find():
+                    users.append({'id': user['id'], 'name': user['name'], 'email': user['email']})
+                    
+                if len(users)==0:
+                    return jsonify({"message": "No user found"})
+                
+                return jsonify(users)
+            except :
+                return jsonify({"message": "No user found"})
 
+    # /users - Creates a new user
+    def post(self):
+        try:
+            # Validate user data
+            user = request.get_json()
+            errors=validate(instance=user,schema=user_schema)
+            if errors:
+                return jsonify({'message': 'Validation error'})
 
-# /- Welcome page
-@app.route('/')
-def home():
-    return jsonify({"message": "Welcome to CoRider"})
+            # check for the existing user
+            user_exist = collection.find_one({'id': user['id']})
+            if user_exist:
+                return jsonify({'message': 'User already exist'})
+            # Insert user data into the database
+            result = collection.insert_one(user)
+            user['_id'] = str(result.inserted_id)
+            return jsonify(user)
+        except: 
+            return jsonify({"message": "Some error occured"})
 
+    # /users/<id> - Updates the user data
+    def put(self, id):
+        try:
+            # Validate user data    
+            user = request.get_json()
+            errors = validate(instance=user,schema=user_schema)
+            if errors:
+                return jsonify({'message': 'Validation error'})
 
-# /users - Returns a list of users
-@app.route('/users', methods=['GET'])
-def get_users():
-    users=[]
-    try: 
-        for user in collection.find():
-            users.append({'id': user['id'], 'name': user['name'], 'email': user['email']})
-            
-        if len(users)==0:
-            return jsonify({"message": "No user found"})
-        
-        return jsonify(users)
-    except :
-        return jsonify({"message": "No user found"})
-
-
-
-# /users/<id> - Returns the single user
-@app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    try:
-        user = collection.find_one({'id': id})
-        if user is None:
-            return jsonify({'message': 'User not found'})
-        return jsonify({'id': user['id'], 'name': user['name'], 'email': user['email']})
-    except: 
-        return jsonify({"message": "No user found"})
-
-
-
-# /users - Creates a new user
-@app.route('/users', methods=['POST'])
-def create_user():
-    try:
-        # Validate user data
-        user = request.get_json()
-        errors=validate(instance=user,schema=user_schema)
-        if errors:
-            return jsonify({'message': 'Validation error'})
-
-        # check for the existing user
-        user_exist = collection.find_one({'id': user['id']})
-        if user_exist:
-            return jsonify({'message': 'User already exist'})
-        # Insert user data into the database
-        result = collection.insert_one(user)
-        user['_id'] = str(result.inserted_id)
-        return jsonify(user)
-    except: 
-        return jsonify({"message": "Some error occured"})
-
-
-
-# /users/<id> - Updates the user data
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-    try:
-        # Validate user data    
-        user = request.get_json()
-        errors = validate(instance=user,schema=user_schema)
-        if errors:
-            return jsonify({'message': 'Validation error'})
-
-        # Update user data in the database
-        result = collection.find_one_and_update({'id': id}, {'$set': user}, return_document=ReturnDocument.AFTER)
-        if result is None:
-            return jsonify({'message': 'User not found'})
-        result['_id'] = str(result['_id'])
-        return jsonify(result)
-    except:
-        return jsonify({"message": "Some error occured"})
+            # Update user data in the database
+            result = collection.find_one_and_update({'id': id}, {'$set': user}, return_document=ReturnDocument.AFTER)
+            if result is None:
+                return jsonify({'message': 'User not found'})
+            result['_id'] = str(result['_id'])
+            return jsonify(result)
+        except:
+            return jsonify({"message": "Some error occured"})
         
 
 
-# /users/<id> - Deletes the user
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    try:
-        result = collection.delete_one({'id': id})
-        if result.deleted_count == 0:
-            return jsonify({'message': 'User not found'})
-        return jsonify({'message': 'User deleted'})
-    except:
-        return jsonify({"message": "User not found"})
+    # /users/<id> - Deletes the user
+    def delete(self, id):
+        try:
+            result = collection.delete_one({'id': id})
+            if result.deleted_count == 0:
+                return jsonify({'message': 'User not found'})
+            return jsonify({'message': 'User deleted'})
+        except:
+            return jsonify({"message": "User not found"})
 
+api.add_resource(User, '/users', '/users/<int:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
